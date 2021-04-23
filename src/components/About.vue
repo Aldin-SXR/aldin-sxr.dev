325bcb6bfab5c91d553045782b09e22b1c161fa7
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2021-04-23 23:56:08 +0200

    Updated the website design with new data.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,54 @@
 <template>
 <div class="section">
     <div class="container">
+
         <div class="has-text-centered mb-6">
-            <p class="title is-3">My Information</p>
-            <p class="subtitle is-5">What are my interests?</p>
+            <p class="title is-3">Who am I?</p>
+            <p class="subtitle is-5">And what are my interests?</p>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="card">
+                <div class="card-content">
+                    <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-48x48">
+                        <img src="@/assets/img/pic.png" alt="My likeness">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">Aldin Kovačević</p>
+                        <p class="subtitle is-6">@aldin-sxr</p>
+                    </div>
+                    </div>
+                    <div class="content">
+                        <p>
+                            I am a Master's student at International Burch University, Department of Information Technologies.
+                            I consider myself hard-working, meticulous, highly motivated and willing to learn and adapt to new technologies.
+                        </p>
+                        <p>
+
+                        </p>
+                    <a href="#">#learn</a> <a href="#">#more</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="content">
+                    <p class="mt-3">Primary programming languages of interest include 
+                        <icon-text icon="php" text="PHP" />, 
+                        <icon-text icon="javascript" text="JavaScript" />, 
+                        <icon-text icon="python" text="Python" /> and 
+                        <icon-text icon="c" text="C/C++" />.
+                    </p>
+                    <p class="mt-3">Primary areas of interest include <b>backend development</b>, <b>decentralized technologies</b> and <b>data science</b>.</p>
+                </div>
+            </div>
         </div>
 
         <div class="has-text-centered mb-6 is-size-5">
-            <p class="mt-3">Primary programming languages of interest include <b>PHP</b>, <b>Python</b> and <b>C/C++</b>.</p>
-            <p class="mt-3">Primary areas of interest include <b>backend development</b>, <b>decentralized technologies</b>, <b>information security</b> and <b>data science</b>.</p>
             <p class="mt-3">よろしくお願いしま。</p>
         </div>
     </div>
@@ -16,8 +56,12 @@
 </template>
 
 <script>
-export default {
+import IconText from '@/components/IconText'
 
+export default {
+    components: {
+        IconText
+    }
 }
 </script>
 
---
+++ src/components/IconText.vue
@@ -0,0 +1,30 @@
+<template>
+<span class="icon-text">
+    <span :class="size" class="icon is-size-4">
+        <i :class="`devicon-${icon}-plain colored`"></i>
+    </span>
+    <span :class="{'has-text-weight-bold': bold}" >{{ text }}</span>
+</span>
+</template>
+
+<script>
+export default {
+    name: 'IconText',
+    props: {
+        icon: String,
+        text: String,
+        size: {
+            type: String,
+            default: ''
+        },
+        bold: {
+            type: Boolean,
+            default: true
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
